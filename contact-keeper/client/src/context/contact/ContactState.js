56376ed7bebc68e1091d38f5ab782eddf1e20691
Ricardas <rihaddas@gmail.com>
2019-11-23 14:23:02 +0200

    Basic react template form

---
+++ contact-keeper/client/src/context/contact/ContactState.js
@@ -0,0 +1,73 @@
+import React, { useReducer } from 'react';
+import uuid from 'uuid';
+import ContactContext from './contactContext';
+import contactReducer from './contactReducer';
+import {
+  ADD_CONTACT,
+  DELETE_CONTACT,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_CONTACT,
+  FILTER_CONTACTS,
+  CLEAR_FILTER
+} from '../types';
+
+const ContactState = props => {
+  const initialState = {
+    contacts: [
+      {
+        id: 1,
+        name: 'Jill Johnson',
+        email: 'jill@gmail.com',
+        phone: '111-111-1111',
+        type: 'personal'
+      },
+      {
+        id: 2,
+        name: 'Sara Watson',
+        email: 'sara@gmail.com',
+        phone: '222-111-3333',
+        type: 'personal'
+      },
+      {
+        id: 3,
+        name: 'Harry Green',
+        email: 'harry@gmail.com',
+        phone: '111-555-9999',
+        type: 'professional'
+      }
+    ]
+  };
+  //state lets us access object in our state and dispatch allows us to dispatch objects to the reducer
+  const [state, dispatch] = useReducer(contactReducer, initialState);
+
+  //Add Contact
+  const addContact = contact => {
+    contact.id = uuid.v4();
+    dispatch({ type: ADD_CONTACT, payload: contact });
+  };
+  //Delete Contact
+
+  //Set Current Contact
+
+  //Clear Current Contact
+
+  //Update Contact
+
+  //Filter Contacts
+
+  //Clear Filter
+
+  return (
+    <ContactContext.Provider
+      value={{
+        contacts: state.contacts,
+        addContact
+      }}
+    >
+      {props.children}
+    </ContactContext.Provider>
+  );
+};
+
+export default ContactState;
