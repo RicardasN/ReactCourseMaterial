661abe8ca4e8cad2167bcdea144d1398c39faacd
Ricardas <rihaddas@gmail.com>
2019-12-02 00:34:18 +0200

    Edit, Delete

--- contact-keeper/client/src/context/contact/ContactState.js
+++ contact-keeper/client/src/context/contact/ContactState.js
@@ -12,8 +12,7 @@ import {
   CLEAR_FILTER,
   CONTACT_ERROR,
   GET_CONTACTS,
-  CLEAR_CONTACTS,
-  CLEAR_ERRORS
+  CLEAR_CONTACTS
 } from '../types';
 
 const ContactState = props => {
@@ -51,8 +50,31 @@ const ContactState = props => {
     }
   };
   //Delete Contact
-  const deleteContact = id => {
-    dispatch({ type: DELETE_CONTACT, payload: id });
+  const deleteContact = async id => {
+    try {
+      await axios.delete(`/api/contacts/${id}`);
+      dispatch({ type: DELETE_CONTACT, payload: id });
+    } catch (error) {
+      dispatch({ type: CONTACT_ERROR, payload: error.response.msg });
+    }
+  };
+  //Update Contact
+  const updateContact = async contact => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+    try {
+      const res = await axios.put(
+        `/api/contacts/${contact._id}`,
+        contact,
+        config
+      );
+      dispatch({ type: UPDATE_CONTACT, payload: res.data });
+    } catch (error) {
+      dispatch({ type: CONTACT_ERROR, payload: error.response.msg });
+    }
   };
   //Set Current Contact
   const setCurrent = contact => {
@@ -61,10 +83,6 @@ const ContactState = props => {
   //Clear Current Contact
   const clearCurrent = () => {
     dispatch({ type: CLEAR_CURRENT });
-  };
-  //Update Contact
-  const updateContact = contact => {
-    dispatch({ type: UPDATE_CONTACT, payload: contact });
   };
   //Filter Contacts
   const filterContacts = text => {
--- contact-keeper/client/src/context/contact/contactReducer.js
+++ contact-keeper/client/src/context/contact/contactReducer.js
@@ -22,7 +22,7 @@ export default (state, action) => {
     case ADD_CONTACT:
       return {
         ...state,
-        contacts: [...state.contacts, action.payload],
+        contacts: [action.payload, ...state.contacts],
         loading: false
       };
     case UPDATE_CONTACT:
@@ -31,7 +31,8 @@ export default (state, action) => {
         contacts: state.contacts.map(contact =>
           contact._id === action.payload._id ? action.payload : contact
         ),
-        loading: false
+        loading: false,
+        current: null
       };
     case DELETE_CONTACT:
       return {
