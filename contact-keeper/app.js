24ca70305649ba0e2183b3040051b774aefde818
Ricardas <rihaddas@gmail.com>
2019-12-02 00:43:44 +0200

    Prepare for deployment

--- contact-keeper/app.js
+++ contact-keeper/app.js
@@ -3,6 +3,7 @@ const connectDB = require('./config/db');
 const authRoutes = require('./routes/auth');
 const usersRoutes = require('./routes/users');
 const contactsRoutes = require('./routes/contacts');
+const path = require('path');
 
 const app = express();
 
@@ -17,6 +18,16 @@ app.use(express.json({ extended: false }));
 app.use('/api/users', usersRoutes);
 app.use('/api/contacts', contactsRoutes);
 app.use('/api/auth', authRoutes);
+
+//Check static assets in production
+if (process.env.NODE_ENV === 'production') {
+  //Set Static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
