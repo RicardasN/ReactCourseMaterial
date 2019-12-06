fdca57f2e348537ea8647e8e5f894aaea4b4fb25
Ricardas <rihaddas@gmail.com>
2019-12-07 00:33:16 +0200

    add and edit logs modal for it-logger

---
+++ it-logger/src/components/layout/Preloader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Preloader = () => {
+  return (
+    <div class='progress blue'>
+      <div class='indeterminate lighten-4'></div>
+    </div>
+  );
+};
+
+export default Preloader;
